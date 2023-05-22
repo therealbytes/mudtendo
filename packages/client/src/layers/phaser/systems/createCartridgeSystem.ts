b9182bd82f975a8a57ff44c7ccd97704d3c5fc20
bytes <therealbyt3s@gmail.com>
2023-05-22 16:00:56 +0200

    feat: prefetch preimages

--- packages/client/src/layers/phaser/systems/createCartridgeSystem.ts
+++ packages/client/src/layers/phaser/systems/createCartridgeSystem.ts
@@ -12,10 +12,10 @@ import {
   pixelCoordToTileCoord,
   tileCoordToPixelCoord,
 } from "@latticexyz/phaserx";
-import { utils } from "ethers";
 
 import { PhaserLayer } from "../createPhaserLayer";
 import { TILE_HEIGHT, TILE_WIDTH, Animations } from "../constants";
+import { hexStringToUint8Array } from "@latticexyz/utils";
 
 export function createCartridgeSystem(layer: PhaserLayer) {
   const {
@@ -31,37 +31,57 @@ export function createCartridgeSystem(layer: PhaserLayer) {
     },
   } = layer;
 
-  function hexToUint8Array(hexString: string): Uint8Array {
-    if (hexString.length % 2 !== 0) {
-      throw new Error("Invalid hex string");
-    }
-    if (hexString.substring(0, 2) === "0x") {
-      hexString = hexString.substring(2);
-    }
-    const bytes = new Uint8Array(hexString.length / 2);
-    for (let i = 0; i < hexString.length; i += 2) {
-      bytes[i / 2] = parseInt(hexString.substring(i, i + 2), 16);
-    }
-    return bytes;
-  }
-
-  const marioStaticHash = hexToUint8Array(
-    "0xebefff5d04586f1d5ba0d052d1a06f2535c5dd92be22c289295442b1048fe872"
-  );
-  const marioDynHash = hexToUint8Array(
-    "0x4123f2d81428f7090218f975b941122f3797aeb8f97bf7d1ef6e87491c920a5c"
-  );
-
-  if (marioStaticHash === undefined || marioDynHash === undefined) {
-    throw new Error("Invalid hashes");
+  function fetchPreimageFromServer(url: string): Promise<Uint8Array> {
+    return fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          if (response.status === 404) {
+            return new Blob(); // Return an empty Blob if the file is not found
+          }
+          throw new Error("Network response was not ok");
+        }
+        return response.blob();
+      })
+      .then((blob) => {
+        return new Promise<Uint8Array>((resolve, reject) => {
+          const reader = new FileReader();
+          reader.onloadend = () => {
+            const arrayBuffer = reader.result as ArrayBuffer;
+            const uint8Array = new Uint8Array(arrayBuffer);
+            resolve(uint8Array);
+          };
+          reader.onerror = () => {
+            reject(new Error("Could not read file as ArrayBuffer."));
+          };
+          reader.readAsArrayBuffer(blob);
+        });
+      });
   }
 
   const cachedHashes = new Set<Uint8Array>();
-  cachedHashes.add(marioStaticHash);
-  cachedHashes.add(marioDynHash);
+
+  const marioStaticHash =
+    "0xebefff5d04586f1d5ba0d052d1a06f2535c5dd92be22c289295442b1048fe872";
+
+  const marioDynHash =
+    "0x4123f2d81428f7090218f975b941122f3797aeb8f97bf7d1ef6e87491c920a5c";
+
+  const preimagesToPreload = [marioStaticHash, marioDynHash];
 
   nes.start();
-  nes.togglePause();
+
+  // TODO: Be less hacking about this
+
+  // We load them from the server instead of the chain so we dev version works
+  // without the custom chain.
+  for (const hash of preimagesToPreload) {
+    fetchPreimageFromServer(`public/preimages/${hash}.bin`).then((preimage) => {
+      if (preimage.length == 0) return;
+      if (cachedHashes.has(preimage)) return;
+      cachedHashes.add(preimage);
+      nes.setPreimage(hexStringToUint8Array(hash), preimage);
+    });
+  }
 
   // Input
 
@@ -71,7 +91,10 @@ export function createCartridgeSystem(layer: PhaserLayer) {
     createCartridge(marioStaticHash, marioDynHash);
   });
 
+  let playing = false;
   input.pointerdown$.subscribe((event) => {
+    if (playing) return;
+    playing = true;
     const pointer = event.pointer;
     if (pointer === undefined) return;
     if (!pointer.isDown) return;
@@ -85,8 +108,8 @@ export function createCartridgeSystem(layer: PhaserLayer) {
     if (!hasComponent(Cartridge, entity)) return;
     const cartridge = getComponentValueStrict(Cartridge, entity);
 
-    const staticHash = hexToUint8Array(cartridge.staticHash as string);
-    const dynHash = hexToUint8Array(cartridge.dynHash as string);
+    const staticHash = hexStringToUint8Array(cartridge.staticHash as string);
+    const dynHash = hexStringToUint8Array(cartridge.dynHash as string);
 
     if (!cachedHashes.has(staticHash)) {
       // ...
@@ -95,19 +118,31 @@ export function createCartridgeSystem(layer: PhaserLayer) {
       // ...
     }
 
-    console.log("Playing cartridge...");
-    nes.togglePause();
-
-    console.log("Setting cartridge...");
     nes.setCartridge(staticHash, dynHash);
+    nes.unpause();
 
     setTimeout(() => {
-      nes.togglePause();
+      nes.pause();
       playCartridge(BigInt(entity), []);
+      playing = false;
     }, 18000);
   });
 
   // Utils
+
+  // function hexToUint8Array(hexString: string): Uint8Array {
+  //   if (hexString.length % 2 !== 0) {
+  //     throw new Error("Invalid hex string");
+  //   }
+  //   if (hexString.substring(0, 2) === "0x") {
+  //     hexString = hexString.substring(2);
+  //   }
+  //   const bytes = new Uint8Array(hexString.length / 2);
+  //   for (let i = 0; i < hexString.length; i += 2) {
+  //     bytes[i / 2] = parseInt(hexString.substring(i, i + 2), 16);
+  //   }
+  //   return bytes;
+  // }
 
   function numToEntity(id: bigint): Entity {
     let idStr = id.toString(16);
@@ -122,6 +157,9 @@ export function createCartridgeSystem(layer: PhaserLayer) {
   function randomInt(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
+  // UI layout
+  // TODO: better cartridge layout
 
   function cartridgePosition(entity: Entity): { x: number; y: number } {
     const cartridge = getComponentValueStrict(Cartridge, entity);
