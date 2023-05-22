90079eb84ed0245ab176653b2f64d5f940f32d50
bytes <therealbyt3s@gmail.com>
2023-05-22 16:00:12 +0200

    feat: add op devnet

--- packages/client/src/mud/supportedChains.ts
+++ packages/client/src/mud/supportedChains.ts
@@ -1,5 +1,26 @@
 import { MUDChain, latticeTestnet } from "@latticexyz/common/chains";
 import { foundry } from "@wagmi/chains";
 
+const opDevnet = {
+  id: 901,
+  name: "Optimism Devnet",
+  network: "op-devnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ether",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    default: {
+      http: ["http://127.0.0.1:8545"],
+      webSocket: ["http://127.0.0.1:8546"],
+    },
+    public: {
+      http: ["http://127.0.0.1:8545"],
+      webSocket: ["http://127.0.0.1:8546"],
+    },
+  },
+};
+
 // If you are deploying to chains other than anvil or Lattice testnet, add them here
-export const supportedChains: MUDChain[] = [foundry, latticeTestnet];
+export const supportedChains: MUDChain[] = [foundry, latticeTestnet, opDevnet];
