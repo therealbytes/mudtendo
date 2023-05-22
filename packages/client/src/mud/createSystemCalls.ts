cb8fec44ec0ad6d7bcc94c35e52a507d3f2f1b89
bytes <therealbyt3s@gmail.com>
2023-05-22 14:07:47 +0200

    feat: wasm nes

--- packages/client/src/mud/createSystemCalls.ts
+++ packages/client/src/mud/createSystemCalls.ts
@@ -1,11 +1,18 @@
+import { ClientComponents } from "./createClientComponents";
 import { SetupNetworkResult } from "./setupNetwork";
 import { ActionStruct } from "contracts/types/ethers-contracts/IWorld";
-import {  utils } from "ethers";
+import { utils } from "ethers";
 
 export type SystemCalls = ReturnType<typeof createSystemCalls>;
 
-export function createSystemCalls({ worldSend }: SetupNetworkResult) {
-  const createCartridge = async (staticHash: utils.BytesLike, dynHash: utils.BytesLike) => {
+export function createSystemCalls(
+  { worldSend }: SetupNetworkResult,
+  { }: ClientComponents
+) {
+  const createCartridge = async (
+    staticHash: utils.BytesLike,
+    dynHash: utils.BytesLike
+  ) => {
     worldSend("createCartridge", [staticHash, dynHash]);
   };
   const playCartridge = async (id: bigint, activity: ActionStruct[]) => {
--- packages/client/src/mud/setup.ts
+++ packages/client/src/mud/setup.ts
@@ -1,5 +1,6 @@
 import { createClientComponents } from "./createClientComponents";
 import { createSystemCalls } from "./createSystemCalls";
+import { createWasm } from "./createWasm";
 import { setupNetwork } from "./setupNetwork";
 
 export type SetupResult = Awaited<ReturnType<typeof setup>>;
@@ -8,9 +9,11 @@ export async function setup() {
   const network = await setupNetwork();
   const components = createClientComponents(network);
   const systemCalls = createSystemCalls(network, components);
+  const wasm = await createWasm();
   return {
     network,
     components,
     systemCalls,
+    wasm,
   };
 }
