f7a6c91081d9a12b7a794621b41a94803086a1ef
bytes <therealbyt3s@gmail.com>
2023-06-04 18:36:11 +0200

    fix: tileset

--- packages/art/tilesets/world.tsx
+++ packages/art/tilesets/world.tsx
@@ -1,34 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.0" name="world" tilewidth="32" tileheight="32" tilecount="3" columns="3">
- <image source="images/world.png" width="96" height="32"/>
+<tileset version="1.9" tiledversion="1.9.0" name="world" tilewidth="32" tileheight="32" tilecount="3" columns="1">
+ <image source="images/world.png" width="32" height="32"/>
  <tile id="0">
-  <properties>
-   <property name="name" value="Grass"/>
-  </properties>
- </tile>
- <tile id="1">
-  <properties>
-   <property name="name" value="Mountain"/>
-  </properties>
- </tile>
- <tile id="2">
-  <properties>
-   <property name="name" value="Forest"/>
-  </properties>
- </tile>
- <tile id="3">
   <properties>
    <property name="name" value="BG"/>
   </properties>
  </tile>
- <tile id="4">
-  <properties>
-   <property name="name" value="Genesis"/>
-  </properties>
- </tile>
- <tile id="5">
-  <properties>
-   <property name="name" value="Cartridge"/>
-  </properties>
- </tile>
-</tileset>
