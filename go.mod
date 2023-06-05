40d06cf5b63763951ed237a4eeb13cb5b0705f62
bytes <therealbyt3s@gmail.com>
2023-06-05 18:31:36 +0200

    feat: add upload script

---
+++ go.mod
@@ -0,0 +1,34 @@
+module github.com/therealbytes/mudtendo
+
+go 1.18
+
+replace github.com/fogleman/nes => github.com/therealbytes/nes v0.0.0-20230529152044-bd334b2d1049
+
+replace github.com/ethereum/go-ethereum => github.com/therealbytes/concrete-geth v0.0.0-20230605155849-07e6a65d56c4
+
+require (
+	github.com/ethereum/go-ethereum v1.11.6
+	github.com/therealbytes/neschain v0.0.0-20230605140054-7899489566dc
+)
+
+require (
+	github.com/StackExchange/wmi v0.0.0-20180116203802-5d049714c4a6 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/deckarep/golang-set/v2 v2.1.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/fogleman/nes v0.0.0-00010101000000-000000000000 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/google/gofuzz v1.1.1-0.20200604201612-c04b05f3adfa // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+	github.com/tklauser/go-sysconf v0.3.10 // indirect
+	github.com/tklauser/numcpus v0.4.0 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ upload.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/big"
+	"os"
+	"time"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/concrete/api"
+	api_test "github.com/ethereum/go-ethereum/concrete/api/test"
+	"github.com/ethereum/go-ethereum/concrete/precompiles"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+var (
+	RpcUrl        = "http://localhost:9545"
+	GasLimit      = uint64(10_000_000)
+	GasPrice      = big.NewInt(1_000_000_000)
+	PrivateKeyHex = "2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6"
+	ChainID       = big.NewInt(901)
+)
+
+func waitForTransaction(client *ethclient.Client, txHash common.Hash) (*types.Receipt, error) {
+	// Create a ticker to check the transaction status at regular intervals
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			// Get the transaction receipt
+			receipt, err := client.TransactionReceipt(context.Background(), txHash)
+			if err != nil {
+				if err.Error() == ethereum.NotFound.Error() {
+					// Transaction is still pending
+					continue
+				}
+				return nil, err
+			}
+
+			// Check if the transaction has been included in a block
+			if receipt != nil {
+				return receipt, nil
+			}
+		}
+	}
+}
+
+func main() {
+	// Check if a command line argument is provided
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run upload.go <file-path>")
+		return
+	}
+
+	// Parse the private key
+	privateKey, err := crypto.HexToECDSA(PrivateKeyHex)
+	if err != nil {
+		panic(err)
+	}
+	fromAddress := crypto.PubkeyToAddress(privateKey.PublicKey)
+
+	// Get the file path from the command line argument
+	filePath := os.Args[1]
+
+	// Read the file contents
+	preimage, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Printf("Error reading file: %s\n", err.Error())
+		return
+	}
+
+	// Create a new API instance
+	API := api.New(api_test.NewMockEVM(api_test.NewMockStateDB()), common.Address{})
+	store := api.NewPersistentBigPreimageStore(API, precompiles.BigPreimageStoreRadix, precompiles.BigPreimageStoreLeafSize)
+
+	// Add the preimage to the store
+	hash := store.AddPreimage(preimage)
+
+	fmt.Println("Preimage hash:", hash.Hex())
+
+	// Get the ABI for the registry
+	ABI := precompiles.BigPreimageRegistry.ABI
+
+	// Create an Ethereum client
+	client, err := ethclient.Dial(RpcUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Prepare the input data for the transaction
+	input, err := ABI.Pack("addPreimage", preimage)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// precompiles.BigPreimageRegistry.SetConfig(precompiles.PreimageRegistryConfig{
+	// 	Enabled:  true,
+	// 	Writable: true,
+	// })
+	// output, err := precompiles.BigPreimageRegistry.Run(API, input)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// retHash := common.BytesToHash(output)
+	// fmt.Println("Preimage hash:", retHash.Hex())
+
+	// Get the nonce for the sender account
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create the transaction
+	transaction := types.NewTransaction(nonce, api.BigPreimageRegistryAddress, common.Big0, GasLimit, GasPrice, input)
+
+	// Sign the transaction
+	signedTransaction, err := types.SignTx(transaction, types.NewEIP155Signer(ChainID), privateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Send the signed transaction
+	err = client.SendTransaction(context.Background(), signedTransaction)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Transaction hash:", signedTransaction.Hash().Hex())
+	fmt.Println("Waiting for receipt...")
+
+	// Wait for the transaction to be included in a block
+	receipt, err := waitForTransaction(client, signedTransaction.Hash())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check if the transaction was successful
+	if receipt.Status == types.ReceiptStatusSuccessful {
+		fmt.Println("Transaction successful.")
+	} else {
+		log.Fatal("Transaction failed.")
+	}
+}
